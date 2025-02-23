d44e01beecc1c809b7c7c78bd7fac321f06e43fa
whu_zhang-shengzhe <2322731680@qq.com>
2025-02-23 16:18:25 +0800

    添加项目文件。

---
+++ c#_1_calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace c__1_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double NumA, NumB;string Symbol;string Result;
+            Console.WriteLine("请输入数字1");
+            NumA =double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入数字2");
+            NumB = double.Parse(Console.ReadLine());
+            Console.WriteLine("请输入运算符(+ - * /)");
+            Symbol = Console.ReadLine();
+
+            switch(Symbol)
+            {
+                case "+":
+                    Result = (NumA + NumB).ToString();
+                    break;
+                case "-":
+                    Result = (NumA - NumB).ToString();
+                    break;
+                case "*":
+                    Result = (NumA * NumB).ToString();
+                    break;
+                case "/":
+                    if (NumB == 0) Result = "错误，被除数不能为零。";
+                    else Result = (NumA / NumB).ToString();
+                    break;
+                default:
+                    Result = "错误，运算符非+ - * /。";
+                    break;
+            }
+             Console.WriteLine("运算结果为:{0}",Result);
+            
+            
+        }
+    }
+}
