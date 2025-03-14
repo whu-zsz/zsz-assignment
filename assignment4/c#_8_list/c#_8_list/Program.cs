0b48dc2b0058b8b12c15f44da1b5e6bcbec2dde6
whu_zhang-shengzhe <2322731680@qq.com>
2025-03-14 16:05:59 +0800

    4

---
+++ assignment4/c#_8_list/c#_8_list/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace c__8_list
+{
+    internal class Program
+    {
+
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }//属性
+            public T Data { get; set; }//属性
+            public Node(T t)
+            {//构造函数
+                Next = null;
+                Data = t;
+            }
+        }
+        public class List<T>
+            {
+                public Node<T> head;
+                public Node<T> tail;
+            
+                public void creat()
+                {
+                    head = tail = null;
+                }
+                public Node<T> Head
+                {
+                    get { return head; }
+                }
+                public void add(T t)
+                {
+                    Node<T> tmp = new Node<T>(t);
+                    if (tail == null)
+                        head = tail = tmp;
+                    else
+                    {
+                        tail.Next = tmp;
+                        tail = tmp;
+                    }
+                }
+                public void Foreach(Action<T> action)
+            {
+                Node<T> now_node = head;
+                while (now_node != null)
+                {
+                    action(now_node.Data);
+                    now_node = now_node.Next;
+                }
+            }
+            } 
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.add(45); list.add(145); list.add(455); list.add(452); list.add(450);
+            Console.WriteLine("输出列表：");
+            list.Foreach(x => Console.WriteLine("{0} ", x));
+            int max = int.MinValue;int min = int.MaxValue;
+            list.Foreach(x => { if (x < min) min = x; });
+            list.Foreach(x => { if (x > max) max = x; });
+            Console.WriteLine("输出最大值：{0}", max);
+            Console.WriteLine("输出最小值：{0}", min);
+            int sum = 0;
+            list.Foreach(x => sum += x);
+            Console.WriteLine("输出总和：{0}", sum);
+
+        }
+    }
+}
---
+++ assignment4/c#_9_clock/c#_9_clock/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace c__9_clock
+{
+    internal class Program
+    {
+        public delegate void clockhandler(object sender);
+        public class clocks
+        {
+           
+
+            public int time_now { get; set; }
+              
+            public int time_clock { get; set; }
+            
+            public clocks(int time_clock_set )
+            {
+                time_now = 0;
+                time_clock = time_clock_set;
+            }
+
+            public event clockhandler Onticks;
+
+            public event clockhandler Onalarm;
+
+            public void start()
+            {
+                while(time_now<time_clock)
+                {
+                    Thread.Sleep(1000);
+                    time_now++;
+                    Onticks(this);
+                    if(time_now==time_clock)
+                    {
+                        Onalarm(this);
+                    }
+                }
+            }
+        }
+        public class form
+        {
+            public clocks _clock = new clocks(0);
+            void ticks_click(object sender)
+            {
+                Console.WriteLine("滴哒");
+            }
+            void alram_click(object sender)
+            {
+                Console.WriteLine("叮铃铃铃");
+            }
+           public form(int time)
+            {
+                clocks clock = new clocks(time);
+                _clock = clock;
+                clock.Onticks += ticks_click;
+                clock.Onalarm += alram_click;
+            }            
+        }
+        static void Main(string[] args)
+        {
+            int times=0;
+            Console.WriteLine("请设置n秒后响铃");
+            times = int.Parse(Console.ReadLine());
+            form tmp = new form(times);
+            Console.WriteLine("闹钟启动，{0}秒后响铃。", times);
+            tmp._clock.start();
+            Console.WriteLine("完成响铃");
+        }
+    }
+}
